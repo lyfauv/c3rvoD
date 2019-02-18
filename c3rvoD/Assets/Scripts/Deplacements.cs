618f0473a49d730d04d5f417fa2b95778cac4b22
Lylia Fauvel <lylfauvel@ensc.fr>
2019-02-18 16:49:50 +0100

    Premiers déplacements

---
+++ c3rvoD/Assets/Scripts/Deplacements.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacements : MonoBehaviour {
+
+    public float speedLeft = 20.0f;//a speed modifier
+    public float speedRight = -20.0f;//a speed modifier
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(0,0, speedRight * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            //makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
+            transform.Rotate(0, 0, speedLeft * Time.deltaTime);
+    }
+}
